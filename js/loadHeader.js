953e2306c9f238a2653dba6cd659b1ac873cf227
JorgeCb12 <jorgeluiscorralesbarraza272@gmail.com>
2025-02-21 11:08:28 -0500

    correciones finales

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(data => {
       document.body.insertAdjacentHTML('afterbegin', data);
       setActiveLink();
+      setupHamburgerMenu();
     })
     .catch(error => console.error('Error al cargar el header:', error));
 });
@@ -17,3 +18,36 @@ function setActiveLink() {
     }
   });
 }
+
+function setupHamburgerMenu() {
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('#header nav ul');
+
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    toggleHamburgerIcon(hamburger);
+  });
+
+  document.addEventListener('click', function(event) {
+    const isClickInside = hamburger.contains(event.target) || navMenu.contains(event.target);
+    if (!isClickInside) {
+      navMenu.classList.remove('show');
+      resetHamburgerIcon(hamburger);
+    }
+  });
+}
+
+function toggleHamburgerIcon(hamburger) {
+  const lines = hamburger.querySelectorAll('div');
+  lines[0].classList.toggle('rotate1');
+  lines[1].classList.toggle('hide');
+  lines[2].classList.toggle('rotate2');
+}
+
+function resetHamburgerIcon(hamburger) {
+  const lines = hamburger.querySelectorAll('div');
+  lines[0].classList.remove('rotate1');
+  lines[1].classList.remove('hide');
+  lines[2].classList.remove('rotate2');
+}
+
